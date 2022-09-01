33a92d077d31669676adf5ea3902218f4fdfa902
michal727 <michalkardas1@gmail.com>
2022-09-01 16:03:17 +0200

    click counter and session storage

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,65 @@
 // selectors
 
-const openPopupBtn = document.querySelector('.open-popup')
+const openPopupBtn = document.querySelector('.open-popup');
 const lightboxContainer = document.querySelector('.lightbox-container');
 const closeBtn = document.querySelector('.close');
+const popup = document.querySelector('.popup-wrapper');
+const resetButton = document.querySelector('.reset')
+let clickCount = document.querySelector('.click-count');
+let clicks = 0;
 
-console.log(lightboxContainer);
-
+if (sessionStorage.clickNumber) {
+    clicks = sessionStorage.getItem("clickNumber");
+} else {
+    clicks = 0;
+}
 
 
 
 //functions
 
-
-
 const showLightbox = () => {
-	lightboxContainer.classList.add('active')
-}
+	lightboxContainer.classList.remove('hide');
+};
 
 const hideLightbox = () => {
-	lightboxContainer.classList.remove('active')
-}
+	lightboxContainer.classList.add('hide');
+};
+
+const addClick = () => {
+    clicks++;
+    clickCount.innerHTML = clicks;
+    if (clicks >= 5) {
+        resetButton.classList.remove('hide');
+        }
+    resetButton.addEventListener('click', ()=> {
+        clicks = 0;
+        clickCount.innerHTML = clicks;
+        resetButton.classList.add('hide');
+    }); 
+    sessionStorage.setItem("clickNumber", clicks);
+    }
 
 
 
 // listeners
 
 openPopupBtn.addEventListener('click', () => {
-    showLightbox();
-    console.log('aaa');
-})
+	showLightbox();
+    addClick();
+});
 
-
+lightboxContainer.addEventListener('click', () => {
+    hideLightbox();
+});
 
 closeBtn.addEventListener('click', () => {
-    hideLightbox();
-    console.log('aaa');
+	hideLightbox();
+});
+
+popup.addEventListener('click', (e) => {
+	e.stopPropagation();
 })
+
+
+
